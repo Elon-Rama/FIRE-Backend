1d2ab279fcd1576a043d668dd2ec9b8e09197d8c
Elon-Rama <ramadevi@elonnativesystem.com>
2024-09-25 10:32:48 +0530

    update-email

--- Model/emailModel.js
+++ Model/emailModel.js
@@ -1,38 +1,5 @@
-// const mongoose = require("mongoose");
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     email: {
-//       type: String,
-//       required: false,
-//     },
-//     otp: {
-//       type: String,
-//       required: false,
-//     },
-//     token: {
-//       type: String,
-//       required: false,
-//     },
-//     refreshToken: {
-//       type: String,
-//       required: false,
-//     },
-//     loggedIn: {
-//       type: Boolean,
-//       default: false,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// const User = mongoose.model("User", userSchema);
-
-// module.exports = User;
-
 const mongoose = require("mongoose");
 
-const Profile = require("../Model/userModel");
 const userSchema = new mongoose.Schema(
   {
     email: {
@@ -55,32 +22,12 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    userProfile: {
-      type: Boolean,
-      default: false,
-    },
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
   },
   { timestamps: true }
 );
 
-userSchema.pre("save", async function (next) {
-  if (this.userId) {
-    try {
-      const profile = await Profile.findOne({ userId: this.userId });
-
-      this.userProfile = profile ? true : false;
-    } catch (err) {
-      return next(err);
-    }
-  }
-  next();
-});
-
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
+
+
