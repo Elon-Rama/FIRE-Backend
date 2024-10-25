5b6682debe1512c3c5c1e987638e22ad581aaaa2
Elon-Rama <ramadevi@elonnativesystem.com>
2024-10-25 14:07:09 +0530

    update

--- Controller/ChildExpenses.js
+++ Controller/ChildExpenses.js
@@ -1,7 +1,85 @@
-const ChildExpenses = require("../Model/ChildExpensesModel");
-const ExpensesMaster = require("../Model/expensesModel");
-const _ = require("lodash");
-const User = require("../Model/emailModel");
+// const ChildExpenses = require("../Model/ChildExpensesModel");
+// const ExpensesMaster = require("../Model/expensesModel");
+// const _ = require("lodash");
+// const User = require("../Model/emailModel");
+
+// // exports.upsert = async (req, res) => {
+// //   //#swagger.tags = ['Child-Expenses']
+// //   const { expensesId, category, userId } = req.body;
+
+// //   try {
+// //     const user = await User.findById(userId);
+// //     if (!user) {
+// //       return res.status(400).json({
+// //         statusCode: "1",
+// //         message: "User not found",
+// //       });
+// //     }
+
+// //     const expenses = await ExpensesMaster.findById(expensesId);
+// //     if (!expenses) {
+// //       return res.status(400).json({
+// //         statusCode: "1",
+// //         message: "Expenses ID does not exist",
+// //       });
+// //     }
+
+// //     const expensesTitle = await ExpensesMaster.findOne(title);
+// //     if(!expensesTitle) {
+// //       return res.status(400).json({
+// //         statusCode: "1",
+// //         message: "Expenses title does not exist",
+// //       });
+// //     }
+
+// //     if (
+// //       !Array.isArray(category) ||
+// //       category.some((cat) => typeof cat !== "string")
+// //     ) {
+// //       return res.status(400).json({
+// //         statusCode: "1",
+// //         message: "Category must be an array of strings",
+// //       });
+// //     }
+
+// //     const existingSubCategory = await ChildExpenses.findOne({ expensesId });
+
+// //     if (existingSubCategory) {
+// //       const updatedSubCategory = await ChildExpenses.findByIdAndUpdate(
+// //         existingSubCategory._id,
+// //         { $addToSet: { category: { $each: category } } },
+// //         { new: true }
+// //       );
+
+// //       return res.status(200).json({
+// //         statusCode: "0",
+// //         message: "SubCategory updated successfully",
+// //         data: updatedSubCategory,
+// //       });
+// //     } else {
+// //       const newSubCategory = new ChildExpenses({
+// //         userId,
+// //         expensesId,
+// //         title,
+// //         category,
+// //         active: true,
+// //       });
+
+// //       await newSubCategory.save();
+
+// //       return res.status(201).json({
+// //         statusCode: "0",
+// //         message: "SubCategory created successfully",
+// //         data: newSubCategory,
+// //       });
+// //     }
+// //   } catch (error) {
+// //     return res.status(500).json({
+// //       statusCode: "1",
+// //       message: error.message,
+// //     });
+// //   }
+// // };
 
 // exports.upsert = async (req, res) => {
 //   //#swagger.tags = ['Child-Expenses']
@@ -10,7 +88,7 @@ const User = require("../Model/emailModel");
 //   try {
 //     const user = await User.findById(userId);
 //     if (!user) {
-//       return res.status(400).json({
+//       return res.status(200).json({
 //         statusCode: "1",
 //         message: "User not found",
 //       });
@@ -18,17 +96,9 @@ const User = require("../Model/emailModel");
 
 //     const expenses = await ExpensesMaster.findById(expensesId);
 //     if (!expenses) {
-//       return res.status(400).json({
+//       return res.status(200).json({
 //         statusCode: "1",
 //         message: "Expenses ID does not exist",
-//       });
-//     }
-
-//     const expensesTitle = await ExpensesMaster.findOne(title);
-//     if(!expensesTitle) {
-//       return res.status(400).json({
-//         statusCode: "1",
-//         message: "Expenses title does not exist",
 //       });
 //     }
 
@@ -36,7 +106,7 @@ const User = require("../Model/emailModel");
 //       !Array.isArray(category) ||
 //       category.some((cat) => typeof cat !== "string")
 //     ) {
-//       return res.status(400).json({
+//       return res.status(200).json({
 //         statusCode: "1",
 //         message: "Category must be an array of strings",
 //       });
@@ -51,16 +121,18 @@ const User = require("../Model/emailModel");
 //         { new: true }
 //       );
 
-//       return res.status(200).json({
+//       return res.status(201).json({
 //         statusCode: "0",
 //         message: "SubCategory updated successfully",
-//         data: updatedSubCategory,
+//         title: expenses.title,
+//         data: {
+//           ...updatedSubCategory._doc,
+//         },
 //       });
 //     } else {
 //       const newSubCategory = new ChildExpenses({
 //         userId,
 //         expensesId,
-//         title,
 //         category,
 //         active: true,
 //       });
@@ -70,7 +142,10 @@ const User = require("../Model/emailModel");
 //       return res.status(201).json({
 //         statusCode: "0",
 //         message: "SubCategory created successfully",
-//         data: newSubCategory,
+//         title: expenses.title,
+//         data: {
+//           ...newSubCategory._doc,
+//         },
 //       });
 //     }
 //   } catch (error) {
@@ -81,6 +156,128 @@ const User = require("../Model/emailModel");
 //   }
 // };
 
+// exports.getAll = async (req, res) => {
+//   //#swagger.tags = ['Child-Expenses']
+//   try {
+//     const { userId } = req.query;
+//     console.log(userId);
+//     const userVerify = await ChildExpenses.findOne({ userId });
+
+//     if (!userId) {
+//       return res.status(200).json({
+//         statusCode: "1",
+//         message: "userId is required",
+//       });
+//     } else if (!userVerify) {
+//       return res.status(200).json({
+//         statusCode: "1",
+//         message: "user can not be found",
+//       });
+//     }
+//     const categories = await ChildExpenses.find().populate(
+//       "expensesId",
+//       "title"
+//     );
+//     res.status(200).json({
+//       statusCode: "0",
+//       message: "subCategories data retrived Successfully",
+//       data: categories,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       statusCode: "1",
+//       message: "Failed to retrived subcategories data",
+//     });
+//   }
+// };
+
+// exports.delete = async (req, res) => {
+//   //#swagger.tags = ['Child-Expenses']
+
+//   try {
+//     const { id } = req.params;
+
+//     if (!id) {
+//       return res.status(200).json({
+//         message: "ChildExpenses ID is required",
+//         statusCode: "1",
+//       });
+//     }
+
+//     const deletedExpense = await ChildExpenses.findByIdAndDelete(id);
+
+//     if (!deletedExpense) {
+//       return res.status(200).json({
+//         message: "ChildExpenses not found",
+//         statusCode: "1",
+//       });
+//     }
+
+//     return res.status(201).json({
+//       message: "ChildExpenses deleted successfully",
+//       data: deletedExpense,
+//       statusCode: "0",
+//     });
+//   } catch (error) {
+//     console.error("Error in deleting child expenses:", error);
+//     return res.status(200).json({
+//       message: "Internal server error",
+//       statusCode: "1",
+//     });
+//   }
+// };
+
+// exports.search = async (req, res) => {
+//   //#swagger.tags = ['Child-Expenses']
+
+//   const searchTerm = req.query.searchTerm;
+
+//   try {
+//     if (!searchTerm) {
+//       return res.status(200).json({
+//         message: "Search term is required",
+//         data: [],
+//         statusCode: "1",
+//       });
+//     }
+
+//     const searchResult = await ChildExpenses.find({
+//       category: { $regex: new RegExp(searchTerm, "i") },
+//     }).populate("expensesId", "title");
+
+//     if (searchResult.length === 0) {
+//       return res.status(200).json({
+//         message: "No matching results found",
+//         data: [],
+//         statusCode: "0",
+//       });
+//     }
+
+//     return res.status(201).json({
+//       message: "ChildExpenses retrieved successfully",
+//       data: searchResult.map((expense) => ({
+//         _id: expense._id,
+//         expensesId: expense.expensesId,
+//         category: expense.category.filter((cat) => cat.includes(searchTerm)),
+//         createdAt: expense.createdAt,
+//         updatedAt: expense.updatedAt,
+//       })),
+//       statusCode: "0",
+//     });
+//   } catch (err) {
+//     console.error("Error in searching child expenses:", err);
+//     return res.status(200).json({
+//       statusCode: "1",
+//       message: "Internal server error",
+//     });
+//   }
+// };
+
+const ChildExpenses = require("../Model/ChildExpensesModel");
+const ExpensesMaster = require("../Model/expensesModel");
+const User = require("../Model/emailModel");
+
+
 exports.upsert = async (req, res) => {
   //#swagger.tags = ['Child-Expenses']
   const { expensesId, category, userId } = req.body;
@@ -88,7 +285,7 @@ exports.upsert = async (req, res) => {
   try {
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(200).json({
+      return res.status(400).json({
         statusCode: "1",
         message: "User not found",
       });
@@ -96,7 +293,7 @@ exports.upsert = async (req, res) => {
 
     const expenses = await ExpensesMaster.findById(expensesId);
     if (!expenses) {
-      return res.status(200).json({
+      return res.status(400).json({
         statusCode: "1",
         message: "Expenses ID does not exist",
       });
@@ -106,7 +303,7 @@ exports.upsert = async (req, res) => {
       !Array.isArray(category) ||
       category.some((cat) => typeof cat !== "string")
     ) {
-      return res.status(200).json({
+      return res.status(400).json({
         statusCode: "1",
         message: "Category must be an array of strings",
       });
@@ -121,7 +318,7 @@ exports.upsert = async (req, res) => {
         { new: true }
       );
 
-      return res.status(201).json({
+      return res.status(200).json({
         statusCode: "0",
         message: "SubCategory updated successfully",
         title: expenses.title,
@@ -133,6 +330,7 @@ exports.upsert = async (req, res) => {
       const newSubCategory = new ChildExpenses({
         userId,
         expensesId,
+        title: expenses.title,
         category,
         active: true,
       });
@@ -142,7 +340,7 @@ exports.upsert = async (req, res) => {
       return res.status(201).json({
         statusCode: "0",
         message: "SubCategory created successfully",
-        title: expenses.title,
+        
         data: {
           ...newSubCategory._doc,
         },
@@ -156,41 +354,42 @@ exports.upsert = async (req, res) => {
   }
 };
 
+// Get all child expenses for a user
 exports.getAll = async (req, res) => {
   //#swagger.tags = ['Child-Expenses']
   try {
     const { userId } = req.query;
-    console.log(userId);
-    const userVerify = await ChildExpenses.findOne({ userId });
 
     if (!userId) {
-      return res.status(200).json({
+      return res.status(400).json({
         statusCode: "1",
         message: "userId is required",
       });
-    } else if (!userVerify) {
-      return res.status(200).json({
+    }
+
+    const userVerify = await ChildExpenses.findOne({ userId });
+    if (!userVerify) {
+      return res.status(404).json({
         statusCode: "1",
-        message: "user can not be found",
+        message: "User not found",
       });
     }
-    const categories = await ChildExpenses.find().populate(
-      "expensesId",
-      "title"
-    );
+
+    const categories = await ChildExpenses.find().populate("expensesId", "title");
     res.status(200).json({
       statusCode: "0",
-      message: "subCategories data retrived Successfully",
+      message: "SubCategories data retrieved successfully",
       data: categories,
     });
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
       statusCode: "1",
-      message: "Failed to retrived subcategories data",
+      message: "Failed to retrieve subcategories data",
     });
   }
 };
 
+// Delete a child expense by ID
 exports.delete = async (req, res) => {
   //#swagger.tags = ['Child-Expenses']
 
@@ -198,7 +397,7 @@ exports.delete = async (req, res) => {
     const { id } = req.params;
 
     if (!id) {
-      return res.status(200).json({
+      return res.status(400).json({
         message: "ChildExpenses ID is required",
         statusCode: "1",
       });
@@ -207,26 +406,27 @@ exports.delete = async (req, res) => {
     const deletedExpense = await ChildExpenses.findByIdAndDelete(id);
 
     if (!deletedExpense) {
-      return res.status(200).json({
+      return res.status(404).json({
         message: "ChildExpenses not found",
         statusCode: "1",
       });
     }
 
-    return res.status(201).json({
+    return res.status(200).json({
       message: "ChildExpenses deleted successfully",
       data: deletedExpense,
       statusCode: "0",
     });
   } catch (error) {
     console.error("Error in deleting child expenses:", error);
-    return res.status(200).json({
+    return res.status(500).json({
       message: "Internal server error",
       statusCode: "1",
     });
   }
 };
 
+// Search for child expenses by category
 exports.search = async (req, res) => {
   //#swagger.tags = ['Child-Expenses']
 
@@ -234,7 +434,7 @@ exports.search = async (req, res) => {
 
   try {
     if (!searchTerm) {
-      return res.status(200).json({
+      return res.status(400).json({
         message: "Search term is required",
         data: [],
         statusCode: "1",
@@ -253,7 +453,7 @@ exports.search = async (req, res) => {
       });
     }
 
-    return res.status(201).json({
+    return res.status(200).json({
       message: "ChildExpenses retrieved successfully",
       data: searchResult.map((expense) => ({
         _id: expense._id,
@@ -266,7 +466,7 @@ exports.search = async (req, res) => {
     });
   } catch (err) {
     console.error("Error in searching child expenses:", err);
-    return res.status(200).json({
+    return res.status(500).json({
       statusCode: "1",
       message: "Internal server error",
     });
