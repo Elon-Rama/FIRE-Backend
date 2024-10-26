67a8df10ff6189be436a0d765d17b413bf42f075
Elon-Rama <ramadevi@elonnativesystem.com>
2024-10-26 11:57:07 +0530

    update-ExpensesAPI

--- Controller/BudgetPlan/expensesallocation.js
+++ Controller/BudgetPlan/expensesallocation.js
@@ -3,8 +3,115 @@ const ExpensesMaster = require("../../Model/expensesModel");
 const ExpensesAllocation = require("../../Model/ExpensesAllocation");
 const ChildExpenses = require("../../Model/ChildExpensesModel");
 
+// exports.upsert = async (req, res) => {
+//   //#swagger.tags = ['Expenses Allocation']
+//   try {
+//     const { userId, titles } = req.body;
+
+//     const user = await User.findById(userId);
+//     if (!user) {
+//       return res.status(404).json({
+//         statuscode: "1",
+//         message: "User not found",
+//       });
+//     }
+
+//     const currentMonth = new Date().toLocaleString("default", {
+//       month: "long",
+//     });
+//     const currentYear = new Date().getFullYear();
+
+//     const existingAllocation = await ExpensesAllocation.findOne({
+//       userId,
+//       month: currentMonth,
+//       year: currentYear,
+//     });
+
+//     let updatedTitles = titles || [];
+//     if (!updatedTitles.length) {
+//       const expensesMaster = await ExpensesMaster.find({ userId });
+//       updatedTitles = expensesMaster
+//         .filter((expense) => expense.active)
+//         .map((expense) => ({
+//           title: expense.title,
+//           amount: 0,
+//           active: expense.active,
+//         }));
+
+//       if (updatedTitles.length === 0) {
+//         return res.status(404).json({
+//           statuscode: "1",
+//           message: "No active expenses found for this user",
+//         });
+//       }
+//     }
+
+//     const existingTitlesMap = existingAllocation
+//       ? new Map(existingAllocation.titles.map((title) => [title.title, title]))
+//       : new Map();
+
+//     updatedTitles.forEach((title) => {
+//       const existingTitle = existingTitlesMap.get(title.title);
+//       if (existingTitle) {
+//         existingTitle.amount = title.amount;
+//       } else {
+//         existingTitlesMap.set(title.title, {
+//           title: title.title,
+//           amount: title.amount,
+//           active: true,
+//         });
+//       }
+//     });
+
+//     const finalTitles = Array.from(existingTitlesMap.values());
+
+//     const updateData = {
+//       userId: user._id,
+//       month: currentMonth,
+//       year: currentYear,
+//       titles: finalTitles,
+//       active: true,
+//     };
+
+//     const updateAllocation = await ExpensesAllocation.findOneAndUpdate(
+//       { userId: user._id, month: currentMonth, year: currentYear },
+//       { $set: updateData },
+//       { new: true, upsert: true }
+//     );
+
+//     const response = {
+//       statuscode: "0",
+//       message: existingAllocation
+//         ? "Expenses Allocation updated successfully"
+//         : "Expenses Allocation created successfully",
+//       userId: user._id,
+//       expensesAllocationId: updateAllocation._id,
+//       Expenses: [
+//         {
+//           month: currentMonth,
+//           year: currentYear,
+//         },
+//         {
+//           titles: finalTitles.map((title) => ({
+//             title: title.title,
+//             amount: title.amount,
+//             active: title.active, // Use title.active here
+//             _id: title._id,
+//           })),
+//         },
+//       ],
+//     };
+
+//     return res.status(201).json(response);
+//   } catch (err) {
+//     console.error(err);
+//     return res.status(500).json({
+//       statuscode: "1",
+//       message: "Internal Server Error",
+//     });
+//   }
+// };
 exports.upsert = async (req, res) => {
-  //#swagger.tags = ['Expenses Allocation']
   try {
     const { userId, titles } = req.body;
 
@@ -65,11 +172,19 @@ exports.upsert = async (req, res) => {
 
     const finalTitles = Array.from(existingTitlesMap.values());
 
+    // Calculate total expenses
+    const totalExpenses = finalTitles.reduce(
+      (total, title) => total + title.amount,
+      0
+    );
+
+    // Include totalExpenses in the update data
     const updateData = {
       userId: user._id,
       month: currentMonth,
       year: currentYear,
       titles: finalTitles,
+      totalExpenses, // Save totalExpenses to the database
       active: true,
     };
 
@@ -86,6 +201,7 @@ exports.upsert = async (req, res) => {
         : "Expenses Allocation created successfully",
       userId: user._id,
       expensesAllocationId: updateAllocation._id,
+      totalExpenses, // Add totalExpenses to the response
       Expenses: [
         {
           month: currentMonth,
@@ -95,7 +211,7 @@ exports.upsert = async (req, res) => {
           titles: finalTitles.map((title) => ({
             title: title.title,
             amount: title.amount,
-            active: expense.active,
+            active: title.active,
             _id: title._id,
           })),
         },
