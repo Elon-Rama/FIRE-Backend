e88ba4389cd3c86860475a4bd0f74c59ad6edeae
Elon-Rama <ramadevi@elonnativesystem.com>
2024-10-29 19:14:15 +0530

    update-Reality

--- Routes/Router/personalRoute.js
+++ Routes/Router/personalRoute.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const ExpensesAllocation = require("../../Controller/BudgetPlan/expensesallocation");
 const { verifyToken } = require("../../Middleware/authMiddleware");
 
-// router.post("/create",verifyToken, ExpensesAllocation.upsert);
-// router.post("/copyPreviousMonth",verifyToken, ExpensesAllocation.copyPreviousMonthData);
-// router.post("/subcategorySaveUpdate",verifyToken, ExpensesAllocation.postSubCategoryValues);
-// router.delete("/delete/:allocationId", verifyToken, ExpensesAllocation.delete);
-// router.post("/getAll", verifyToken, ExpensesAllocation.getAll);
-// router.get("/:userId/:month/:year",  ExpensesAllocation.getById);
-
-router.post("/create", ExpensesAllocation.upsert);
-router.delete("/delete/:allocationId", ExpensesAllocation.delete);
-router.get("/getAll", ExpensesAllocation.getAll);
+router.post("/create",verifyToken, ExpensesAllocation.upsert);
+router.post("/copyPreviousMonth",verifyToken, ExpensesAllocation.copyPreviousMonthData);
+router.post("/subcategorySaveUpdate",verifyToken, ExpensesAllocation.postSubCategoryValues);
+router.delete("/delete/:allocationId", verifyToken, ExpensesAllocation.delete);
+router.post("/getAll", verifyToken, ExpensesAllocation.getAll);
 router.get("/:userId/:month/:year",  ExpensesAllocation.getById);
+
+// router.post("/create", ExpensesAllocation.upsert);
+// router.delete("/delete/:allocationId", ExpensesAllocation.delete);
+// router.get("/getAll", ExpensesAllocation.getAll);
+// router.get("/:userId/:month/:year",  ExpensesAllocation.getById);
 
 module.exports = router;
