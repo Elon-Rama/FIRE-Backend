a81ce34a0a6d2dcac0a7013efa4648f12012e947
Elon-Rama <ramadevi@elonnativesystem.com>
2024-11-01 13:37:58 +0530

    update

--- Routes/Router/personalRoute.js
+++ Routes/Router/personalRoute.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const ExpensesAllocation = require("../../Controller/BudgetPlan/expensesallocation");
 const { verifyToken } = require("../../Middleware/authMiddleware");
 
-// router.post("/create",verifyToken, ExpensesAllocation.upsert);
-// router.post("/copyPreviousMonth",verifyToken, ExpensesAllocation.copyPreviousMonthData);
-// router.post("/subcategorysaveupdate",verifyToken,ExpensesAllocation.postSubCategoryValues);
-// router.delete("/delete/:allocationId", verifyToken, ExpensesAllocation.delete);
-// router.post("/getAll", verifyToken,ExpensesAllocation.getAll);
-// router.get("/:userId/:month/:year",  ExpensesAllocation.getById);
-
-router.post("/create",ExpensesAllocation.upsert);
-router.post("/copyPreviousMonth",ExpensesAllocation.copyPreviousMonthData);
-router.post("/subcategorysaveupdate",ExpensesAllocation.postSubCategoryValues);
-router.delete("/delete/:allocationId", ExpensesAllocation.delete);
-router.post("/getAll", ExpensesAllocation.getAll);
+router.post("/create",verifyToken, ExpensesAllocation.upsert);
+router.post("/copyPreviousMonth",verifyToken, ExpensesAllocation.copyPreviousMonthData);
+router.post("/subcategorysaveupdate",verifyToken,ExpensesAllocation.postSubCategoryValues);
+router.delete("/delete/:allocationId", verifyToken, ExpensesAllocation.delete);
+router.post("/getAll", verifyToken,ExpensesAllocation.getAll);
 router.get("/:userId/:month/:year",  ExpensesAllocation.getById);
+
+// router.post("/create",ExpensesAllocation.upsert);
+// router.post("/copyPreviousMonth",ExpensesAllocation.copyPreviousMonthData);
+// router.post("/subcategorysaveupdate",ExpensesAllocation.postSubCategoryValues);
+// router.delete("/delete/:allocationId", ExpensesAllocation.delete);
+// router.post("/getAll", ExpensesAllocation.getAll);
+// router.get("/:userId/:month/:year",  ExpensesAllocation.getById);
 
 module.exports = router;
