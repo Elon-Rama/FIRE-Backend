3d068e770c0b1981f29ab651019127bc7af7e0d2
Elon-Rama <ramadevi@elonnativesystem.com>
2024-11-27 16:00:18 +0530

    update

--- Routes/Router/debtRoute.js
+++ Routes/Router/debtRoute.js
@@ -4,12 +4,12 @@ const DebtClearance = require('../../Controller/debtController');
 const { verifyToken } = require("../../Middleware/authMiddleware");
 
 
-// router.post('/create',verifyToken, DebtClearance.createDebt);
-// router.get('/all',verifyToken,DebtClearance.getAllDebts);
-// router.post('/payemi',verifyToken,DebtClearance.payEMI);
+router.post('/create',verifyToken, DebtClearance.createDebt);
+router.get('/all',verifyToken,DebtClearance.getAllDebts);
+router.post('/payemi',verifyToken,DebtClearance.payEMI);
 
-router.post('/create', DebtClearance.createDebt);
-router.get('/all',DebtClearance.getAllDebts);
-router.post('/payemi',DebtClearance.payEMI);
+// router.post('/create', DebtClearance.createDebt);
+// router.get('/all',DebtClearance.getAllDebts);
+// router.post('/payemi',DebtClearance.payEMI);
 
 module.exports = router;
