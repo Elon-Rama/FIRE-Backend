137288139074de6d190bcf62d87aa5f05b2d31ba
Elon-Rama <ramadevi@elonnativesystem.com>
2024-12-11 12:08:42 +0530

    update-index

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const swaggerUi = require("swagger-ui-express");
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(
     session({
         secret: process.env.SESSION_SECRET || 'yoursecret',
